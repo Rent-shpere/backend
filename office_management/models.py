585a02774639923acd7d1e3830aafbd670f0bb38
iamsamienda <samuelendale7373@gmail.com>
2025-03-11 23:45:09 +0300

    Add initial models and migrations for office management and user management

--- office_management/models.py
+++ office_management/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+from datetime import timedelta
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+class Office(models.Model):
+    officeNo = models.CharField(max_length=10)
+    area = models.DecimalField(max_digits=5, decimal_places=2)
+    floorNo = models.IntegerField()
+    status = models.Choices('available', 'rented', 'under_maintenance')
+    
+    
+class Rental(models.Model):
+    tenantId = models.ForeignKey(User, on_delete=models.CASCADE)
+    officeId = models.ForeignKey(Office, on_delete=models.CASCADE)
+    startDate = models.DateField()
+    endDate = models.DateField()
+    monthlyRent = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    
+class Payment(models.Model):
+    rentalId = models.ForeignKey(Rental, on_delete=models.CASCADE)
+    dueDate = models.DateField()
+    paidDate = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.Choices('paid', 'unpaid', 'overdue')
+    
+    
+class MaintenanceRequest(models.Model):
+    tenantId = models.ForeignKey(User, on_delete=models.CASCADE)
+    officeId = models.ForeignKey(Office, on_delete=models.CASCADE)
+    description = models.TextField()
+    status = models.Choices('open', 'in_progress', 'resolved')
+    requestDate = models.DateField(auto_now=True)
+    
+    
+class Announcement(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    createdAt = models.DateField(auto_now=True)
+    expiredDate = models.DateField(default=timezone.now() + timedelta(days=10))
--- rentsphere/settings.py
+++ rentsphere/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
     'office_management',
     'rest_framework',
     'rest_framework_simplejwt',
@@ -128,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
